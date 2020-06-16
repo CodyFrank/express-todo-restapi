d51d6c352f3a38b739a4556594e5c53dcef45ca4
Cody Frank <cody.frank30@gmail.com>
2020-06-16 14:14:40 -0400

    connected to the database

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res)=>{
     res.send("<h1>Welcome to Todo's app</h1>")
 })
+
+mongoose.connect(process.env.DB_CONNECTION, 
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+    }, console.log("connected to db")
+)
 
 
 app.listen(3000, ()=>console.log("The server is running..."))
