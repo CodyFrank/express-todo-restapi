b10147cf721dd3d28252b332da05de5871aa9ced
Cody Frank <cody.frank30@gmail.com>
2020-06-16 13:45:49 -0400

    creates root route

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
-
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res)=>{
+    res.send("hello world")
+})
+
 
 app.listen(3000, ()=>console.log("The server is running..."))
