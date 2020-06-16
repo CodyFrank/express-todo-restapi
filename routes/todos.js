5603ec28073db354e636861152f466d5ce13718b
Cody Frank <cody.frank30@gmail.com>
2020-06-16 14:57:50 -0400

    create index and create routes

--- routes/todos.js
+++ routes/todos.js
@@ -3,8 +3,26 @@ const router = express.Router()
 const Todo = require('../models/Todo')
 
 // todo index
-router.get('/', (req, res) => {
-    res.send("todo index")
+router.get('/', async (req, res) => {
+    try{
+        const todos = await Todo.find()
+        res.json(todos)
+    }catch(err){
+        res.json({ message: err})
+    }
+})
+
+router.post('/', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title,
+        description: req.body.description,   
+    })
+    try{
+        const savedTodo = await todo.save()
+        res.json(savedTodo)
+    }catch(err){
+        res.json({message: err})
+    }
 })
 
 module.exports = router
